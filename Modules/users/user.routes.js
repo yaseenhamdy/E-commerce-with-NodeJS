77394674eed994afec838c669e0150372754e692
Ibrahim-Elsayed-27 <himahema858@gmail.com>
2026-03-12 22:25:38 +0200

    feat: added authorization middleware and enableed getting all users for admin only

--- Modules/users/user.routes.js
+++ Modules/users/user.routes.js
@@ -3,9 +3,10 @@ import { listUsers, signIn, signUp, verifyAccount } from "./user.controller.js";
 import checkEmail from "../../Middleware/checkEmail.js";
 import hashPassword from "../../Middleware/hashPassword.js";
 import verifyToken from "../../Middleware/verifyToken.js";
+import authorize from "../../Middleware/authorization.js";
 let userRoutes = express.Router();
 
-userRoutes.get("/users", verifyToken, listUsers);
+userRoutes.get("/users", verifyToken, authorize("admin"), listUsers);
 userRoutes.post("/auth/signin", checkEmail, signIn);
 userRoutes.post("/auth/signup", checkEmail, hashPassword, signUp);
 userRoutes.get("/auth/verify/:email", verifyAccount);
