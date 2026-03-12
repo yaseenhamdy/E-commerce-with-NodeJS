381e0e37e221c13e9b0d6edaf186bdb8caf36220
Ibrahim-Elsayed-27 <himahema858@gmail.com>
2026-03-12 22:16:16 +0200

    feat: added authentication for listing users

--- Modules/users/user.routes.js
+++ Modules/users/user.routes.js
@@ -2,9 +2,10 @@ import express from "express";
 import { listUsers, signIn, signUp, verifyAccount } from "./user.controller.js";
 import checkEmail from "../../Middleware/checkEmail.js";
 import hashPassword from "../../Middleware/hashPassword.js";
+import verifyToken from "../../Middleware/verifyToken.js";
 let userRoutes = express.Router();
 
-userRoutes.get("/users", listUsers);
+userRoutes.get("/users", verifyToken, listUsers);
 userRoutes.post("/auth/signin", checkEmail, signIn);
 userRoutes.post("/auth/signup", checkEmail, hashPassword, signUp);
 userRoutes.get("/auth/verify/:email", verifyAccount);
