dcea62cda1fa330e10c93929b55b068ff36e958b
yaseenhamdy <yaseenhamdy74@gmail.com>
2026-03-13 18:02:51 +0200

    add create cart and get user cart endpoints / validation for create cart / validation middleware

--- Modules/carts/cart.routes.js
+++ Modules/carts/cart.routes.js
@@ -1,9 +1,12 @@
 import express from "express";
-
-import { getCartItems } from "./cart.controller.js";
+import verifyToken from '../../Middleware/verifyToken.js'
+import { addToCart , getUserCart } from "./cart.controller.js";
+import validationMiddleware from "../../Middleware/validationMiddleware.js";
+import {createCartSchema} from "../carts/cart.validation.js" 
 
 let cartRoutes = express.Router();
 
-cartRoutes.get("/cart" , getCartItems)
+cartRoutes.get("/cart/:userid" ,verifyToken ,getUserCart)
+cartRoutes.post("/cart" ,verifyToken,validationMiddleware(createCartSchema) ,addToCart)
 
 export default cartRoutes
